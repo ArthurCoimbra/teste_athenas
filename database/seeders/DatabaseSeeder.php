f628341879fd4d81c60efa534fd9e77cd31984ab
ArthurCoimbra <arthurplank22@gmail.com>
2020-11-12 16:36:29 -0300

    Adicionado o arquivo seed e exclusão de alguns arquivos desnecessários

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +14,28 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+      DB::table('categorias')->insert([
+        ['nome' => 'Admin', 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s")],
+        ['nome' => 'Gerente', 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s")],
+        ['nome' => 'Normal', 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s")]
+      ]);
+
+      DB::table('pessoas')->insert([
+        ['nome' => 'Jorge da Silva', 'email' => 'jorge@terra.com.br', 'categoria_id' => 1, 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s")],
+        ['nome' => 'Flavia Monteiro', 'email' => 'flavia@globo.com', 'categoria_id' => 2, 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s")],
+        ['nome' => 'Marcos Frota Ribeiro', 'email' => 'ribeiro@gmail.com', 'categoria_id' => 2, 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s")],
+        ['nome' => 'Raphael Souza Santos', 'email' => 'rsantos@gmail.com', 'categoria_id' => 1, 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s")],
+        ['nome' => 'Pedro Paulo Mota', 'email' => 'ppmota@gmail.com', 'categoria_id' => 1, 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s")],
+        ['nome' => 'Winder Carvalho da Silva', 'email' => 'winder@hotmail.com', 'categoria_id' => 3, 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s")],
+        ['nome' => 'Maria da Penha Albuquerque', 'email' => 'mpa@hotmail.com', 'categoria_id' => 3, 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s")],
+        ['nome' => 'Rafael Garcia Souza', 'email' => 'rgsouza@hotmail.com', 'categoria_id' => 3, 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s")],
+        ['nome' => 'Tabata Costa', 'email' => 'tabata_costa@gmail.com', 'categoria_id' => 2, 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s")],
+        ['nome' => 'Ronil Camarote', 'email' => 'camarote@terra.com.br', 'categoria_id' => 1, 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s")],
+        ['nome' => 'Joaquim Barbosa', 'email' => 'barbosa@globo.com', 'categoria_id' => 1, 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s")],
+        ['nome' => 'Eveline Maria Alcantra', 'email' => 'ev_alcantra@gmail.com', 'categoria_id' => 2, 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s")],
+        ['nome' => 'João Paulo Vieira', 'email' => 'jpvieria@gmail.com', 'categoria_id' => 1, 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s")],
+        ['nome' => 'Carla Zamborlini', 'email' => 'zamborlini@terra.com.br', 'categoria_id' => 3, 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s")],
+
+      ]);
     }
 }
